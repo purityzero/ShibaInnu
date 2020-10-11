927f7dc0d88b7a8dba8079a2ce66edf10cd76fee
Glory <youngtae@treenod.com>
2020-10-11 15:36:11 +0900

    최조 커밋

---
+++ ShibaMaker/Assets/Scripts/IntroScene/IntroManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroManager : MonoBehaviour
+{
+    public Button StartButton;
+    public Button CollectionButton;
+    public Button ExitButton;
+
+    public void Start()
+    {
+        StartButton?.onClick.AddListener(() => GameStart());
+        CollectionButton?.onClick.AddListener(() => Collection());
+        ExitButton?.onClick.AddListener(() => Exit());
+    }
+
+    private void Collection()
+    {
+
+    }
+
+    private void Exit()
+    {
+        Application.Quit();
+#if !UNITY_EDITOR
+        System.Diagnostics.Process.GetCurrentProcess().Kill();
+#endif
+    }
+
+    private void GameStart()
+    {
+
+    }
+}
---
+++ ShibaMaker/Assets/Scripts/Utiltity/DotweenPathMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DotweenPathMove : MonoBehaviour
+{
+    public Ease EaseType;
+    public List<Vector3> WayPointList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DOTween.Init(false, true, LogBehaviour.ErrorsOnly);
+
+        gameObject.transform.DOPath(WayPointList.ToArray(), 6.0f, PathType.CatmullRom).SetLookAt(Vector3.zero).SetEase(EaseType);
+    }
+
+
+}
