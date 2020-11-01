a19090d9a800dfd0e8eb5308ac431504cae98ae6
glory <glory@treenod.com>
2020-11-01 17:39:49 +0900

    리워드 생성중이에요~ 부엉이가 한마리 있어요~~!!

---
+++ ShibaMaker/Assets/Scripts/InGameScene/Reward.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum RewardType
+{
+    None = -1,
+    Food,
+    Dress,
+    Favor
+
+}
+
+public enum Dress
+{
+    None,
+}
+
+public enum Food
+{
+    None,
+}
+
+public enum Favor
+{
+    None,
+}
+
+public class Reward : MonoBehaviour
+{
+    public Image RewardImage;
+    public Text RewardText;
+    private RewardType rewardType;
+    public const int FOOD_MAX_COUNT = 5;
+
+
+    int FoodCount = 1;
+
+    public void Init(RewardType _rewardType)
+    {
+        rewardType = _rewardType;
+
+        if (rewardType == RewardType.Food)
+            FoodCount = Random.Range(1, FOOD_MAX_COUNT + 1);
+
+    }
+
+    public int SelectRewardItemIndex()
+    {
+        // rewardType string 값으로 받아서
+        // string을 enum Type 으로 변경, 변경 후 idx 판별 
+        int idx = Random.Range(0, (int)rewardType);
+        return idx;
+    }
+
+
+}
---
+++ ShibaMaker/Assets/Scripts/InGameScene/RewardPopup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RewardPopup : MonoBehaviour
+{
+
+    [SerializeField] Reward PrefabRewardObj;
+    VerticalLayoutGroup LayoutGroup;
+
+    public void Init(List<RewardType> rewardTypes)
+    {
+
+        for (int i = 0; i < rewardTypes.Count; i++)
+        {
+            Reward rewardItem = Instantiate<Reward>(PrefabRewardObj, LayoutGroup.transform);
+            rewardItem.Init(rewardTypes[i]);
+        }
+                
+    }
+}
+    
